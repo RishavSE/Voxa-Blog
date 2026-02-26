63cca0cfc42f5eee5fdc8006bd1791a5af9d2350
Rishav Kumar Singh <rishavse06@gmail.com>
2026-02-26 14:29:06 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { register, login, resetPassword } = require("../controllers/authController");
 
-// Auth routes
 router.post("/register", register);
 router.post("/login", login);
-router.post("/reset-password", resetPassword); // Add this line
+router.post("/reset-password", resetPassword); 
 
 module.exports = router;
